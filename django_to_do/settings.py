77e217668b1640b32c6d5ad1d6b231758ecb1862
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-06-28 10:50:47 +0000

    filter tasks by status on to do list

--- django_to_do/settings.py
+++ django_to_do/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
     '.herokuapp.com',
     '8000-hussal-djangowarriorsto-6swwmva6y1n.ws-eu114.gitpod.io',
     '8000-hussal-djangowarriorsto-pfky2z2c52d.ws.codeinstitute-ide.net',
-    '8000-hussal-djangowarriorsto-5vz967jljk8.ws.codeinstitute-ide.net',
+    '8000-hussal-djangowarriorsto-7mlip39q7ij.ws.codeinstitute-ide.net',
     'localhost',
     '127.0.0.1',
 ]
@@ -100,7 +100,7 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-hussal-djangowarriorsto-6swwmva6y1n.ws-eu114.gitpod.io",
-    "https://8000-hussal-djangowarriorsto-5vz967jljk8.ws.codeinstitute-ide.net",
+    "https://8000-hussal-djangowarriorsto-7mlip39q7ij.ws.codeinstitute-ide.net",
     "https://8000-hussal-djangowarriorsto-pfky2z2c52d.ws.codeinstitute-ide.net",
     "https://*.herokuapp.com"
 ]
